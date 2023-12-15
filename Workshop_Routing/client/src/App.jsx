66dc2a896a52e1bc85388c38d94fc8152f0064ac
Lyubomir Tasev <lubomirtasev_1998@abv.bg>
2023-12-15 18:52:03 +0200

    Added a Game Create page

--- Workshop_Routing/client/src/App.jsx
+++ Workshop_Routing/client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import GamesList from "./components/games-list/GamesList";
+import CreateGame from "./components/game-create/CreateGame";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element= {<Home />} />
         <Route path="/games" element={<GamesList />} />
+        <Route path="/games/create" element={<CreateGame />} />
       </Routes>
       
     </div>
