3bc5f36754e6302a94fc73c0814d67b32cd86073
Lyubomir Tasev <lubomirtasev_1998@abv.bg>
2023-12-15 21:22:21 +0200

    Added login and register pages

--- Workshop_Routing/client/src/App.jsx
+++ Workshop_Routing/client/src/App.jsx
@@ -4,6 +4,8 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import GamesList from "./components/games-list/GamesList";
 import CreateGame from "./components/game-create/CreateGame";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element= {<Home />} />
         <Route path="/games" element={<GamesList />} />
         <Route path="/games/create" element={<CreateGame />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
       
     </div>
