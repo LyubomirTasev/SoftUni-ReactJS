bc3efc5dc25d326afa1859aab3415768b0ca634a
Lyubomir Tasev <lubomirtasev_1998@abv.bg>
2023-12-16 23:14:56 +0200

    modified requester

--- Workshop_Routing/client/src/services/gameService.js
+++ Workshop_Routing/client/src/services/gameService.js
@@ -1,22 +1,15 @@
-import { request } from "../lib/request";
+import * as request from "../lib/request";
 
 const baseUrl = 'http://localhost:3030/jsonstore/games'
 
 export const getAll = async () => {
-    const result = await request('GET', baseUrl);
+    const result = await request.get(baseUrl);
 
     return Object.values(result);
 }
 
 export const create = async (gameData) => {
-    const response = await fetch(baseUrl, {
-        method: 'POST', 
-        headers: {
-            'content-type': 'application/json'                                                // we're creating the game on the server here
-        },
-        body: JSON.stringify(gameData) // we should return the data in a string format          and the server returns our game
-    });
+    const result = await request.post(baseUrl, gameData); // and then return it again as a Javascript object
 
-    const result = await response.json(); // and then return it again as a Javascript object
     return result;
 }
