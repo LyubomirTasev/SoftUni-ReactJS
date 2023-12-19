094912319c009130b16486c965cec3a3c8503c56
Lyubomir Tasev <lubomirtasev_1998@abv.bg>
2023-12-19 08:41:31 +0200

    editet SuccessPage styling

--- E-Commerse-React/client/src/Components/SuccessPage/SuccessPage.jsx
+++ E-Commerse-React/client/src/Components/SuccessPage/SuccessPage.jsx
@@ -2,10 +2,9 @@ import './SuccessPage.css'
 
 export default function SuccessPage() {
   function generateRandom10DigitNumber() {
-    // Generate a random decimal between 0 (inclusive) and 1 (exclusive)
     let randomDecimal = Math.random();
 
-    // Scale the random decimal to a 10-digit integer
+    
     let random10DigitNumber =
       Math.floor(randomDecimal * 9000000000) + 1000000000;
 
@@ -13,9 +12,9 @@ export default function SuccessPage() {
   }
 
   return (
-    <>
+    <div className='wrapper'>
       <h1>Thank You for the order {generateRandom10DigitNumber()}!</h1>
       <p>Please check the confirmation we sent to your email.</p>
-    </>
+    </div>
   );
 }
