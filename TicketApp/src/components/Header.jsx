a0b4d7fc509578d6a66c07cadda570602e8b9d4b
Lyubomir Tasev <lubomirtasev_1998@abv.bg>
2024-07-07 23:23:59 +0300

    Initial commit

---
+++ TicketApp/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+        <>
+        <h1>Let there be live</h1>
+        <h3>Your next best-night-ever is waiting</h3>
+        <input type="text" placeholder="What do you want to search live?"></input>
+        </>
+    )
+}
