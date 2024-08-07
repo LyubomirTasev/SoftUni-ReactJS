00a906f1e9b0204ba51732bd2e4171ed305fec1c
Lyubomir Tasev <lubomirtasev_1998@abv.bg>
2024-08-07 16:47:04 +0300

    That was the first project I started, it didn't workout so well so i had to delete it

--- TicketApp/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-    return(
-        <>
-        <h1>Let there be live</h1>
-        <h3>Your next best-night-ever is waiting</h3>
-        <input type="text" placeholder="What do you want to search live?"></input>
-        </>
-    )
-}
