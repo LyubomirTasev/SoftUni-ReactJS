e8096b12b4343b4493ff7afd8ea5b74108819dfb
Lyubomir Tasev <lubomirtasev_1998@abv.bg>
2024-08-07 21:51:58 +0300

    added json file with the new items

--- E-Commerce_Final Project/client/src/Pages/Shop.jsx
+++ E-Commerce_Final Project/client/src/Pages/Shop.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
 import NewCollections from '../Components/NewCollections/NewCollections'
 import NewsLetter from '../Components/NewsLetter/NewsLetter'
+import * as itemsAPI from '../api/items';
 
 const Shop = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    itemsAPI.getAll()
+    .then(result => setItems(result));
+  }, []);
+  
   return (
     <div>
       <Hero/>
