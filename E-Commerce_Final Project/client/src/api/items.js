39e75a0457d2729ac5437f5bb384d102114e490a
Lyubomir Tasev <lubomirtasev_1998@abv.bg>
2024-08-07 22:14:24 +0300

    List items

--- E-Commerce_Final Project/client/src/api/items.js
+++ E-Commerce_Final Project/client/src/api/items.js
@@ -2,4 +2,10 @@ import * as request from "./requester"
 
 const BASE_URL = 'http://localhost:3030/jsonstore/create-items';
 
-export const getAll = async () => request.get(BASE_URL);
+export const getAll = async () => {
+    const result = await request.get(BASE_URL);
+
+    const newItems = Object.values(result);
+
+    return newItems;
+}
